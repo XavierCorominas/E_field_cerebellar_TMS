8d52a5006a146d742506874384f4b522edecb9be
XavierCorominas <109666629+XavierCorominas@users.noreply.github.com>
2025-05-09 09:31:49 +0200

    Update Head_model.m

--- Head_model.m
+++ Head_model.m
@@ -22,7 +22,7 @@ filename = ['v_CRETMS_01_002_S5_T1w_mp2rage_UNI_Images_MPRAGEised_biascorrected.
 filename_2 = ['v_CRETMS_01_002_S2_T1w_mp2rage_INV1.nii']; %change this by your INV1 image
 
 % Bother previous images need to be in  the same folter: on my case they are at F:\BRAINMAG_Li-rTMS\DATA\BrainNMAG_05_025\MRI
-path_to_mri = ['\\l2export\iss02.cenir\analyse\irm\users\martina.bracco\cRETMS_MRIs\2022_04_29_CRETMS_01_002\Head_model\'];% change this by your root folder
+path_to_mri = ['...\2022_04_29_CRETMS_01_002\Head_model\'];% change this by your root folder
 
 
 % Then, Run the SIMNIBS charm protocol in the terminal to segment the head tissues. 
@@ -32,6 +32,6 @@ cd(path_to_mri)
 clc;
 [status,cmdout] = system(['cd ',path_to_mri],'-echo');
 pwd
-[status,cmdout] = system(['C:\Users\vridhi.rohira\Documents\MATLAB\simnibs_env\Scripts\charm',' ',subject_id,' ',filename,' ',filename_2,  ' --forceqform --forcerun'], '-echo');
+[status,cmdout] = system(['...MATLAB\simnibs_env\Scripts\charm',' ',subject_id,' ',filename,' ',filename_2,  ' --forceqform --forcerun'], '-echo');
 
 %%
